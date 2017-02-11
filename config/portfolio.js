61af5be5fbc9298cdd24b16bb537318919f8cc53
radhe12 <radhey.gor@gmail.com>
2017-02-10 22:20:04 -0500

    Update portfolio.js

--- config/portfolio.js
+++ config/portfolio.js
@@ -1,1 +1,13 @@
-hello
+let mongoose = require('mongoose');
+
+// create a model class
+let portfolioSchema = mongoose.Schema({
+    name: String,
+    rating: Number,
+    cost: Number
+},
+{
+  collection: "portfolio"
+});
+
+module.exports = mongoose.model('portfolio', portfolioSchema);
